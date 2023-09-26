dc307a2ecb2356edd2f738cf91f0c2af9512804f
toudidel <toudidel@gmail.com>
2023-09-26 13:01:50 +0200

    Enabled CORS

---
+++ fincon-starter-app/src/main/java/eu/fincon/starterapp/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package eu.fincon.starterapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebSecurityConfig {
+
+  @Bean
+  public WebMvcConfigurer corsMappingConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
+      }
+    };
+  }
+}
